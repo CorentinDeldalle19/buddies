2cab04846a3f2830b2cf82691f926199368a4d33
Corentin Deldalle <151193169+CorentinDeldalle19@users.noreply.github.com>
2025-03-04 18:15:00 +0100

    Final

--- api/models/index.js
+++ api/models/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -7,7 +5,6 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
@@ -16,14 +13,11 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+const db = {};
+
 fs.readdirSync(__dirname)
     .filter(file => {
-        return (
-            file.indexOf('.') !== 0 &&
-            file !== basename &&
-            file.slice(-3) === '.js' &&
-            file.indexOf('.test.js') === -1
-        );
+        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
     })
     .forEach(file => {
         const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
